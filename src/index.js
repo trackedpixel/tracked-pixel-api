96b1150c75e0db81ee4d6d466c904475cc3023b3
Brian McCarthy <brian.mcc@rthy.org>
2017-11-18 20:05:42 -0500

    create new api for voting on names. being lazy by reusing the same tracking pixel api

--- src/index.js
+++ src/index.js
@@ -187,6 +187,66 @@ app.post('/pusher/auth', (req, res) => {
   res.send(auth);
 });
 
+app.get('/names', (req, res, next) => {
+  const db = req.app.locals.db;
+
+  db.collection("names")
+    .find()
+    .toArray()
+    .then(items => res.json(items))
+    .catch(next);
+});
+
+app.post('/names', (req, res, next) => {
+  const db = req.app.locals.db;
+
+  let newName = {
+    ip: getIpAddress(req),
+    name: req.body.name,
+    votes: 1,
+    createdTime: new Date()
+  };
+
+  // todo: validate body of request.
+
+  db.collection("names")
+    .insertOne(newName)
+    .then((resp) => {
+      let doc = resp.ops[0];
+
+      res.send(doc).status(201).end();
+    })
+    .catch(next);
+});
+
+app.put('/names/:id/up-vote', (req, res, next) => {
+  const db = req.app.locals.db;
+
+  db.collection("names")
+    .findOneAndUpdate(
+    { _id: ObjectID(req.params.id) },
+    { $inc: { "votes": 1 } },
+    { returnOriginal: false })
+    .then((doc) => {
+      return res.send(doc.value);
+    })
+    .catch((err) => console.log('error updating votes...', err));
+});
+
+app.put('/names/:id/down-vote', (req, res, next) => {
+  const db = req.app.locals.db;
+
+  db.collection("names")
+    .findOneAndUpdate(
+    { _id: ObjectID(req.params.id) },
+    { $inc: { "votes": -1 } },
+    { returnOriginal: false })
+    .then((doc) => {
+      return res.send(doc.value);
+    })
+    .catch((err) => console.log('error updating votes...', err));
+});
+
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
